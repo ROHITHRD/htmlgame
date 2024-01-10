1bc934feb4cfdcefaa56f00088cdf69bafdfbf69
ROHITHRD <amilinenirohith770@gmail.com>
2024-01-10 13:27:01 +0530

    car game js

--- game.js
+++ game.js
@@ -1,79 +1,28 @@
 const canvas = document.getElementById('gameCanvas');
 const ctx = canvas.getContext('2d');
-const box = 20;
 
-let snake = [{x: 9 * box, y: 10 * box}];
-let food = {x: Math.floor(Math.random() * 19 + 1) * box, y: Math.floor(Math.random() * 19 + 1) * box};
-let score = 0;
+const carWidth = 60;
+const carHeight = 100;
+let carX = canvas.width / 2 - carWidth / 2;
+let carY = canvas.height - carHeight - 20;
 
-let d;
-
-document.addEventListener('keydown', direction);
-
-function direction(event) {
-    if (event.keyCode === 37 && d !== 'RIGHT') {
-        d = 'LEFT';
-    } else if (event.keyCode === 38 && d !== 'DOWN') {
-        d = 'UP';
-    } else if (event.keyCode === 39 && d !== 'LEFT') {
-        d = 'RIGHT';
-    } else if (event.keyCode === 40 && d !== 'UP') {
-        d = 'DOWN';
-    }
+function drawCar() {
+    ctx.fillStyle = 'blue';
+    ctx.fillRect(carX, carY, carWidth, carHeight);
 }
 
-function collision(head, array) {
-    return array.some(segment => {
-        return segment.x === head.x && segment.y === head.y;
-    });
-}
-
-function draw() {
+function drawGame() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-    for (let i = 0; i < snake.length; i++) {
-        ctx.fillStyle = (i === 0) ? 'green' : 'lightgreen';
-        ctx.fillRect(snake[i].x, snake[i].y, box, box);
-
-        ctx.strokeStyle = 'darkgreen';
-        ctx.strokeRect(snake[i].x, snake[i].y, box, box);
-    }
-
-    ctx.fillStyle = 'red';
-    ctx.fillRect(food.x, food.y, box, box);
-
-    let snakeX = snake[0].x;
-    let snakeY = snake[0].y;
-
-    if (d === 'LEFT') snakeX -= box;
-    if (d === 'UP') snakeY -= box;
-    if (d === 'RIGHT') snakeX += box;
-    if (d === 'DOWN') snakeY += box;
-
-    if (snakeX === food.x && snakeY === food.y) {
-        score++;
-        food = {x: Math.floor(Math.random() * 19 + 1) * box, y: Math.floor(Math.random() * 19 + 1) * box};
-    } else {
-        snake.pop();
-    }
-
-    const newHead = {x: snakeX, y: snakeY};
-
-    if (
-        snakeX < box || snakeX > 19 * box ||
-        snakeY < box || snakeY > 19 * box ||
-        collision(newHead, snake)
-    ) {
-        clearInterval(game);
-        alert('Game Over!');
-        window.location.reload(); // Reload the page to restart the game
-    }
-
-    snake.unshift(newHead);
-
-    ctx.fillStyle = 'white';
-    ctx.font = '45px Arial';
-    ctx.fillText(score, 2 * box, 1.6 * box);
+    drawCar();
+    requestAnimationFrame(drawGame);
 }
 
-const game = setInterval(draw, 100);
+document.addEventListener('keydown', (event) => {
+    if (event.key === 'ArrowLeft' && carX > 0) {
+        carX -= 10;
+    } else if (event.key === 'ArrowRight' && carX < canvas.width - carWidth) {
+        carX += 10;
+    }
+});
+
+drawGame();
